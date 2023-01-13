caa404eeeb7ffe81d0dbea252688d4dd9e52fe37
Ahmad Eyamin <ahmadeyamin@gmail.com>
2023-01-13 22:01:28 +0600

    hero section done

--- src/components/home/hero-section/hero-section.tsx
+++ src/components/home/hero-section/hero-section.tsx
@@ -14,6 +14,11 @@ export const HeroSection = ({ className }: HeroSectionProps) => {
                 <h2 className={styles.HeroIntroText}>
                     <span className={styles.HeroIntroName}>Hello, I&apos;m Eyamin, </span>Creative designer based in Bangladesh
                 </h2>
+                <p className={`text-white ${styles.IntroBio}`}>A web designer and developer in Bangladesh. I specialized in Responsive Web Design and Backend technology </p>
+            </div>
+            <div className={styles.HeroCTAWrapper}>
+                <a href="/" className="btn">Get In Touch</a>
+                <a href="/" className="btn outline">View All Works</a>
             </div>
         </div>
     </div>;
